5caed4ea570caef744a1c4c127b7d87e720685a6
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-24 17:10:40 +0900

    UI_onoff_otherScnee

--- Assets/JKH/01.Scripts/KH_UIManager.cs
+++ Assets/JKH/01.Scripts/KH_UIManager.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 
 public class KH_UIManager : MonoBehaviour
 {
-    public GameObject AudiSpec;
+    //자동차 스펙 메뉴
+    public GameObject carSpec;
+    //자동차 세부사항
+    public GameObject details;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //details.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -21,15 +25,18 @@ public class KH_UIManager : MonoBehaviour
 
     public void OnAudi()
     {
-        AudiSpec.SetActive(true);
+        //인식하고 알림 뜨면 요거 뜨게한다...
+        carSpec.SetActive(true);
     }
     
     public void OnClickAudiDetails()
     {
-        //아우디 Detail.SetActive(True)
+        //버튼 누르면 세부사항 켜진다
+        details.SetActive(true);
     }
     public void OnClickParking()
     {
+        //버튼누르면 씬 전환
         SceneManager.LoadScene("JHW_MiniGame1");
     }
 }
