39b55432fd261153f0e8a4625636d0b039a7cd2f
jeedd95 <80206325+jeedd95@users.noreply.github.com>
2021-11-04 16:01:23 +0900

    .

---
+++ Assets/JHW/01.Script/JHW_SoundManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JHW_SoundManager : MonoBehaviour
+{
+    public static JHW_SoundManager instance;
+    AudioSource MyAudio;
+
+    public AudioClip engineOn;
+    public AudioClip Crash;
+    public AudioClip accel;
+    public AudioClip back;
+    public AudioClip BGM;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayEngineOn()
+    {
+        MyAudio.PlayOneShot(engineOn);
+    }
+    public void PlayCrash()
+    {
+        MyAudio.PlayOneShot(Crash);
+    }
+    public void PlayAccel()
+    {
+        MyAudio.PlayOneShot(accel);
+    }
+    public void PlayBack()
+    {
+        MyAudio.PlayOneShot(back);
+    }
+    public void PlayBGM()
+    {
+        MyAudio.PlayOneShot(BGM);
+    }
+    
+}
