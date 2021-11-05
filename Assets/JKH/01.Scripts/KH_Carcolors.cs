a9592cd6264e04910ce8898ddb04d94d3c08da94
jkh5039 <borm3403@naver.com>
2021-11-05 12:15:30 +0900

    cas

--- Assets/JKH/01.Scripts/KH_Carcolors.cs
+++ Assets/JKH/01.Scripts/KH_Carcolors.cs
@@ -4,32 +4,56 @@ using UnityEngine;
 
 public class KH_Carcolors : MonoBehaviour
 {
-    //차 종류들
-    public GameObject carColor;
-    public GameObject[] carColors;
+    //차 종류들1
+    //public GameObject carColor;
+    //public GameObject[] carColors;
     MeshRenderer mr;
 
     public Material[] carMaterials;
+    int num;
     void Start()
     {
         mr = GetComponent<MeshRenderer>();
         //mr.materials[1] = carMaterials[0];
+        //MultiImage.instance.isGenesis = true;
+
     }
 
-    
+
     void Update()
     {
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            mr.materials[1].color = Color.black;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            mr.materials[1].color = Color.white;
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            mr.materials[1].color = Color.gray;
+        }
+
         if (MultiImage.instance.isAudi)
         {
-            mr.materials[1] = carMaterials[0];
+            mr.materials[1].color = Color.gray;
+            // mr.materials[1] = carMaterials[1]; //silver
         }
-        else if (MultiImage.instance.isBenz)
+        if (MultiImage.instance.isBenz)
         {
-            mr.materials[1] = carMaterials[0];
+            mr.materials[1].color = Color.black;
+            //mr.materials[1] = carMaterials[0]; //black
         }
-        else if (MultiImage.instance.isGenesis)
+        if (MultiImage.instance.isGenesis)
         {
-            mr.materials[1] = carMaterials[0];
+            mr.materials[1].color = Color.white;
+            //mr.materials[1] = carMaterials[2]; //white
         }
+
+        
+
+
     }
 }
